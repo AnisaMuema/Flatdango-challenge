ea61ac9ad3b7ac7899fa50ca5eec3ededceb48d2
anisamuema <anisa.muema.school@gmail.com>
2023-10-10 16:25:00 +0300

    HTML and CSS

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-fetch("https://www.gstatic.com/tv/thumb/v22vodart/2157/p2157_v_v8_ab.jpg", ()=>{
-    const configurationObject = {
-        method: "GET",
-        "Contetent-Type": "Application.json",
-    }
-})
-.then ((res) => res.json)
+
+
+
+fetch('http://localhost:3000/films')
+.then(res => res.json())
 .then((data) => console.log(data))
