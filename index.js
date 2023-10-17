cae2e0404c9ab8e4be67ac17f77efb7d55755cff
anisamuema <anisa.muema.school@gmail.com>
2023-10-17 12:41:53 +0300

    Updated Flatdango

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
-const button = document.getElementById("button")
-const filmsContainer = document.getElementById("Film-bar")
+const url =  'http://localhost:3000/films'
+const picture = document.getElementById('image')  
+const Time = document.getElementById('duration')
+const describe = document.getElementById('description')        
+const film = document.getElementById('title')
+const button = document.querySelector('.buy-ticket')
+const video = document.getElementById('movies-bar')
+const capacity = document.getElementById('capacity')
+const show = document.getElementById('showtime')
+const sold = document.getElementById('available-ticket')
+const availableTickets = capacity - sold  
 
-// button.addEventListener("click", (e)=> {
-//     e.preventDefault   
-
-// })
-
-// DOM Render Functions
-
-
-// Initial render
 // Get data and render our films to the DOM
-let getFilms = () => {
-    fetch("http://localhost:3000/films")
+let getFilms = (id = 1) => {
+    fetch(`${url}/${id}`)
     .then((res) => res.json())
-    .then((data) => console.log(data))
-
+    .then((data) => {
+        picture.setAttribute("src", data.poster);
+        film.innerText = data.title;
+        describe.innerText = data.description;
+        Time.innerText = 'duration'+" " + data.runtime; 
+        capacity.innerText ='capacity '+ data.capacity; 
+        sold.innerText = "available tickets "+ (data.capacity-data.tickets_sold);
+        show.innerText = "show time "+ data.showtime;
+    })
     
 }
-getFilms();
+getFilms()
 
+let allFilms = () => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => returnFilms(data))
+}
 
+allFilms()
+
+let returnFilms = (array) =>{
+    const filmsHtml = array.map(function(character){
+        return `<span onclick ="getFilms(${character.id})">${character.title}</span>`
+    })
+    film.innerHTML = filmsHtml.join('')
+}
+
+button.addEventListener('click', function() {
+    console.log('clicked');
+     // console.log(sold.innerText.split(' ')[2]);
+     let ticket = sold.innerText.split(' ')[2] -1 
+     
+     sold.innerText ="available tickets "+ ticket
+     
+     
+ });
