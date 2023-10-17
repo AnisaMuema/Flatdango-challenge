f55f333de948664597ef5ac5f827bd96d211569f
anisamuema <anisa.muema.school@gmail.com>
2023-10-17 11:53:21 +0300

    Rendering data onto DOM

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const button = document.getElementById("button")
+const filmsContainer = document.getElementById("Film-bar")
 
-button.addEventListener("click", (e)=> {
-    e.preventDefault
-     
+// button.addEventListener("click", (e)=> {
+//     e.preventDefault   
 
-})
+// })
+
+// DOM Render Functions
 
 
-fetch('http://localhost:3000/films')
-.then(res => res.json())
-.then((data) => console.log(data))
+// Initial render
+// Get data and render our films to the DOM
+let getFilms = () => {
+    fetch("http://localhost:3000/films")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+
+    
+}
+getFilms();
+
+
